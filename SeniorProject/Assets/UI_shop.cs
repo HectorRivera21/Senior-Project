3b4145cea1c25819a30bd0d19f3ce5acc6049da2
Mrodr98 <mark.rodriguez05@utrgv.edu>
2024-03-19 14:37:35 -0500

    Started shop

---
+++ SeniorProject/Assets/UI_shop.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using UnityEngine;
+using TMPro;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine.UI;
+
+public class UI_shop : MonoBehaviour
+{
+    private Transform container;
+    private Transform shopItemTemplate;
+
+    private void Awake()
+    {
+        container = transform.Find("Container");
+        shopItemTemplate = container.Find("shopItemTemplate");
+        shopItemTemplate.gameObject.SetActive(false);
+    }
+
+    private void Start()
+    {
+        //CreateItemButton()
+    }
+
+    private void CreateItemButton(Sprite itemSprite, string itemName, int itemCost, int positionIndex)
+    {
+        Transform shopItemTransform = Instantiate(shopItemTemplate, container);
+        RectTransform shopItemRectTransform = shopItemTransform.GetComponent<RectTransform>();
+
+        float shopItemHeight = 30f;
+        shopItemRectTransform.anchoredPosition = new Vector2(0, -shopItemHeight * positionIndex);
+
+        shopItemTransform.Find("itemName").GetComponent<TextMeshProUGUI>().SetText(itemName);
+        shopItemTransform.Find("costText").GetComponent<TextMeshProUGUI>().SetText(itemCost.ToString());
+        shopItemTransform.Find("itemImage").GetComponent<UnityEngine.UI.Image>().sprite = itemSprite;
+
+
+
+    }
+}
