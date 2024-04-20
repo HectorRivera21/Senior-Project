43a7347f01d76c8b2d5b2b737a75edcfea9b3119
Damian Gomez <damianjosiahgomez79@gmail.com>
2024-04-20 11:38:45 -0500

    Doors lock/open when enemies

--- SeniorProject/Assets/Enemies/common_scripts/EnemiesSpawns.cs
+++ SeniorProject/Assets/Enemies/common_scripts/EnemiesSpawns.cs
@@ -7,10 +7,12 @@ public class EnemiesSpawns : MonoBehaviour
 
     bool have_spawn =false;
     SpawnE spawnE;
+    PlayerAttr playerAttr;
     // Start is called before the first frame update
     void Start()
     {
         spawnE = GetComponentInParent<SpawnE>();
+        playerAttr = GameObject.FindWithTag("Player").GetComponent<PlayerAttr>();
     //    Debug.Log(gameObject.name + " local spce:" + gameObject.transform.position); 
         //StartCoroutine(SpawnEnemies());
     }
@@ -28,7 +30,7 @@ public class EnemiesSpawns : MonoBehaviour
         // float randomNumbery = Random.Range(-3f, 3f);
 
         // Wait for 5 seconds
-        yield return new WaitForSeconds(1);
+        yield return new WaitForSeconds(.5f);
         for(int i=0; i<5;i++){
             float randomNumberx = Random.Range(-2f, 2f);
             float randomNumbery = Random.Range(-2f, 2f);
@@ -36,21 +38,27 @@ public class EnemiesSpawns : MonoBehaviour
             switch (e_number){
             case 0:
                 spawnE.SpawnSlime1(new Vector3(randomNumberx,randomNumbery, 0f), gameObject.transform.parent.position);
+                playerAttr.enemiesToKilled += 1;
                 break;
             case 1:
                 spawnE.SpawnSlime2(new Vector3(randomNumberx,randomNumbery, 0f), gameObject.transform.parent.position);
+                playerAttr.enemiesToKilled += 1;
                 break;
             case 2:
                 spawnE.SpawnSlime3(new Vector3(randomNumberx,randomNumbery, 0f), gameObject.transform.parent.position);
+                playerAttr.enemiesToKilled += 1;
                 break;
             case 3:
                 spawnE.SpawnGaurdain1(new Vector3(randomNumberx,randomNumbery, 0f), gameObject.transform.parent.position);
+                playerAttr.enemiesToKilled += 1;
                 break;
             case 4:
                 spawnE.SpawnGaurdain2(new Vector3(randomNumberx,randomNumbery, 0f), gameObject.transform.parent.position);
+                playerAttr.enemiesToKilled += 1;
                 break;
             case 5:
                 spawnE.SpawnGaurdain3(new Vector3(randomNumberx,randomNumbery, 0f), gameObject.transform.parent.position);
+                playerAttr.enemiesToKilled += 1;
                 break;
             default:
                 Debug.Log("nada");
@@ -65,7 +73,7 @@ public class EnemiesSpawns : MonoBehaviour
 
 
     private void OnTriggerEnter2D(Collider2D other) {
-        Debug.Log("SPAWN DA FOOLS");
+        // Debug.Log("SPAWN DA FOOLS");
         if(other.tag == "Player" && have_spawn == false){
             Debug.Log("SPAWN DA FOOLS In if");
             StartCoroutine(SpawnEnemies());
